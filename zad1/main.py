0bf3a7d31fbdbd4bf48889cc989da6aa4490c489
236630 <236630@edu.p.lodz.pl>
2022-03-12 16:04:50 +0100

    Menu is finished, algorithm will stop when eps or max amount of iterations is reached.

--- zad1/main.py
+++ zad1/main.py
@@ -11,7 +11,7 @@
 import numpy
 from matplotlib import pyplot as plt
 
-eps = 1
+
 
 def horner(tabl, n, x):                 #   tabl - tablica wspolczynnikow wielomianu
     wynik = tabl[0]                     #   n - dlugosc tablicy
@@ -89,6 +89,53 @@ def sieczne(fun, pocz, kon, eps):
     print("Liczba uzytych iteracji: ", iteracje)
     return pkt
 
+def bisekcja_iteracje(fun, pocz, kon, iter):
+    p = pocz
+    k = kon
+    i = pocz
+    iteracje = 1
+    fun_eps = 10e-10
+    if fun(p)*fun(k) > 0:
+        print("Podany przedzial jest bledny!")
+        return
+    while iteracje <= iter:
+        srodek = (p + k)/2
+        if i < kon:
+            i = i + 1
+        if abs(fun(srodek)) < fun_eps:
+            return srodek
+        if fun(p)*fun(srodek) > 0:
+            p = srodek
+        else:
+            k = srodek
+        iteracje += 1
+    print("Liczba uzytych iteracji: ", iteracje)
+    return srodek
+
+
+def sieczne_iteracje(fun, pocz, kon, iter):
+    p = pocz
+    k = kon
+    i = pocz
+    iteracje = 1
+    fun_eps = 10e-10
+    if fun(p)*fun(k) > 0:
+        print("Podany przedzial jest bledny!")
+        return
+    while iteracje <= iter:
+        pkt = (-1 * p * fun(p) * (k - p)) / (fun(k) - fun(p))  # pkt przeciecia siecznej z osia OX przez pocz i kon przedzialu
+        if i < kon:
+            i = i + 1
+        if abs(fun(pkt)) < fun_eps:
+            return pkt
+        if fun(p)*fun(pkt) > 0:
+            p = pkt
+        else:
+            k = pkt
+        iteracje += 1
+    print("Liczba uzytych iteracji: ", iteracje)
+    return pkt
+
 
 def fun_wybor():
     funkcja = int
@@ -139,34 +186,102 @@ def przedzial():
     return pocz, kon
 
 
+# def warunek():
+#     wybor = int
+#     try:
+#         wybor = int(input("Algorytm ma zakonczyc sie:\n"
+#                           "1. Po osiagnieciu okreslonego eps,\n"
+#                           "2. Po osiagnieciu okreslinej liczby iteracji.\n"
+#                           "Wybor: "))
+#     except ValueError:
+#         print("Wprowadzono nieodpowiedni znak.")
+#     if wybor < 1 or wybor > 2:
+#         warunek()
+#     if wybor == 1:
+#         eps = double
+#         try:
+#             eps = double(input("Podaj wartosc eps: "))
+#         except ValueError:
+#             print("Wprowadzono nieodpowiedni znak.")
+#         return eps
+#
+#     elif wybor == 2:
+
 def menu(fun, metoda):
     if metoda < 1 or metoda > 2:
         print("Nieprawidlowy wybor metody.")
         met_wybor(fun)
-    pocz, kon = przedzial()
-    if metoda == 1:
-        if fun == 1:
-            bisekcja(fun1(), pocz, kon, eps)
-        elif fun == 2:
-            bisekcja(fun2(), pocz, kon, eps)
-        elif fun == 3:
-            bisekcja(fun3(), pocz, kon, eps)
-        elif fun == 4:
-            bisekcja(fun4(), pocz, kon, eps)
-        elif fun == 5:
-            bisekcja(fun5(), pocz, kon, eps)
-    elif metoda == 2:
-        if fun == 1:
-            sieczne(fun1(), pocz, kon, eps)
-        elif fun == 2:
-            sieczne(fun2(), pocz, kon, eps)
-        elif fun == 3:
-            sieczne(fun3(), pocz, kon, eps)
-        elif fun == 4:
-            sieczne(fun4(), pocz, kon, eps)
-        elif fun == 5:
-            sieczne(fun5(), pocz, kon, eps)
-    return
+
+    warunek = int
+    try:
+        warunek = int(input("Algorytm ma zakonczyc sie gdy:\n"
+                            "1.Zostanie osiagnieta podana liczba iteracji,\n"
+                            "2.Zostanie osiagnieta podana wartosc eps.\n"
+                            "Wybor: "))
+    except ValueError:
+        print("Wprowadzono nieodpowiedni znak.")
+    if warunek < 1 or warunek > 2:
+        menu(fun,metoda)
+
+    if warunek == 1:
+        iteracje = int
+        try:
+            iteracje = int(input("Podaj maksymalna liczbe iteracji: "))
+        except ValueError:
+            print("Wprowadzono nieodpowiedni znak.")
+        pocz, kon = przedzial()
+        if metoda == 1:
+            if fun == 1:
+                bisekcja_iteracje(fun1(), pocz, kon, iteracje)
+            elif fun == 2:
+                bisekcja_iteracje(fun2(), pocz, kon, iteracje)
+            elif fun == 3:
+                bisekcja_iteracje(fun3(), pocz, kon, iteracje)
+            elif fun == 4:
+                bisekcja_iteracje(fun4(), pocz, kon, iteracje)
+            elif fun == 5:
+                bisekcja_iteracje(fun5(), pocz, kon, iteracje)
+        elif metoda == 2:
+            if fun == 1:
+                sieczne_iteracje(fun1(), pocz, kon, iteracje)
+            elif fun == 2:
+                sieczne_iteracje(fun2(), pocz, kon, iteracje)
+            elif fun == 3:
+                sieczne_iteracje(fun3(), pocz, kon, iteracje)
+            elif fun == 4:
+                sieczne_iteracje(fun4(), pocz, kon, iteracje)
+            elif fun == 5:
+                sieczne_iteracje(fun5(), pocz, kon, iteracje)
+
+    elif warunek == 2:
+        eps = numpy.double
+        try:
+            eps = numpy.double(input("Podaj wartosc eps: "))
+        except ValueError:
+            print("Wprowadzono nieodpowiedni znak.")
+        pocz, kon = przedzial()
+        if metoda == 1:
+            if fun == 1:
+                bisekcja(fun1(), pocz, kon, eps)
+            elif fun == 2:
+                bisekcja(fun2(), pocz, kon, eps)
+            elif fun == 3:
+                bisekcja(fun3(), pocz, kon, eps)
+            elif fun == 4:
+                bisekcja(fun4(), pocz, kon, eps)
+            elif fun == 5:
+                bisekcja(fun5(), pocz, kon, eps)
+        elif metoda == 2:
+            if fun == 1:
+                sieczne(fun1(), pocz, kon, eps)
+            elif fun == 2:
+                sieczne(fun2(), pocz, kon, eps)
+            elif fun == 3:
+                sieczne(fun3(), pocz, kon, eps)
+            elif fun == 4:
+                sieczne(fun4(), pocz, kon, eps)
+            elif fun == 5:
+                sieczne(fun5(), pocz, kon, eps)
 
 
 def main():
