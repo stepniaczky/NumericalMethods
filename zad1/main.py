d985f53f6f2e20ec327e23c4f13e26939fbebe0f
236630 <236630@edu.p.lodz.pl>
2022-03-15 19:56:04 +0100

    Major error fixed - still some small fixes are required

--- zad1/main.py
+++ zad1/main.py
@@ -219,7 +219,7 @@ def menu(funk, metoda):
     except ValueError:
         print("Wprowadzono nieodpowiedni znak.")
     if warunek < 1 or warunek > 2:
-        menu(funk,metoda)
+        menu(funk, metoda)
 
     if warunek == 1:
         iteracje = int
@@ -230,26 +230,26 @@ def menu(funk, metoda):
         pocz, kon = przedzial()
         if metoda == 1:
             if funk == 1:
-                bisekcja_iteracje(fun1(pocz), pocz, kon, iteracje)
+                bisekcja_iteracje(fun1, pocz, kon, iteracje)
             elif funk == 2:
-                bisekcja_iteracje(fun2(pocz), pocz, kon, iteracje)
+                bisekcja_iteracje(fun2, pocz, kon, iteracje)
             elif funk == 3:
-                bisekcja_iteracje(fun3(pocz), pocz, kon, iteracje)
+                bisekcja_iteracje(fun3, pocz, kon, iteracje)
             elif funk == 4:
-                bisekcja_iteracje(fun4(pocz), pocz, kon, iteracje)
+                bisekcja_iteracje(fun4, pocz, kon, iteracje)
             elif funk == 5:
-                bisekcja_iteracje(fun5(pocz), pocz, kon, iteracje)
+                bisekcja_iteracje(fun5, pocz, kon, iteracje)
         elif metoda == 2:
             if funk == 1:
-                sieczne_iteracje(fun1(pocz), pocz, kon, iteracje)
+                sieczne_iteracje(fun1, pocz, kon, iteracje)
             elif funk == 2:
-                sieczne_iteracje(fun2(pocz), pocz, kon, iteracje)
+                sieczne_iteracje(fun2, pocz, kon, iteracje)
             elif funk == 3:
-                sieczne_iteracje(fun3(pocz), pocz, kon, iteracje)
+                sieczne_iteracje(fun3, pocz, kon, iteracje)
             elif funk == 4:
-                sieczne_iteracje(fun4(pocz), pocz, kon, iteracje)
+                sieczne_iteracje(fun4, pocz, kon, iteracje)
             elif funk == 5:
-                sieczne_iteracje(fun5(pocz), pocz, kon, iteracje)
+                sieczne_iteracje(fun5, pocz, kon, iteracje)
 
     elif warunek == 2:
         eps = float
@@ -260,26 +260,26 @@ def menu(funk, metoda):
         pocz, kon = przedzial()
         if metoda == 1:
             if funk == 1:
-                bisekcja(fun1(pocz), pocz, kon, eps)
+                bisekcja(fun1, pocz, kon, eps)
             elif funk == 2:
-                bisekcja(fun2(pocz), pocz, kon, eps)
+                bisekcja(fun2, pocz, kon, eps)
             elif funk == 3:
-                bisekcja(fun3(pocz), pocz, kon, eps)
+                bisekcja(fun3, pocz, kon, eps)
             elif funk == 4:
-                bisekcja(fun4(pocz), pocz, kon, eps)
+                bisekcja(fun4, pocz, kon, eps)
             elif funk == 5:
-                bisekcja(fun5(pocz), pocz, kon, eps)
+                bisekcja(fun5, pocz, kon, eps)
         elif metoda == 2:
             if funk == 1:
-                sieczne(fun1(pocz), pocz, kon, eps)
+                sieczne(fun1, pocz, kon, eps)
             elif funk == 2:
-                sieczne(fun2(pocz), pocz, kon, eps)
+                sieczne(fun2, pocz, kon, eps)
             elif funk == 3:
-                sieczne(fun3(pocz), pocz, kon, eps)
+                sieczne(fun3, pocz, kon, eps)
             elif funk == 4:
-                sieczne(fun4(pocz), pocz, kon, eps)
+                sieczne(fun4, pocz, kon, eps)
             elif funk == 5:
-                sieczne(fun5(pocz), pocz, kon, eps)
+                sieczne(fun5, pocz, kon, eps)
 
 
 def main():
