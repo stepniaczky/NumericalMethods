b9f7b91909078d398599149d6ae014d0c251ddc2
Mateusz <236630@edu.p.lodz.pl>
2022-03-19 20:13:38 +0100

    Fixes in algorithms.py

--- zad1/algorithms.py
+++ zad1/algorithms.py
@@ -3,7 +3,7 @@ from functions import fun1, fun2, fun3, fun4, fun5
 
 
 def bisekcja(f, p, k, cond, limiter):
-    if (f(p) * f(k)) < 0:
+    if (f(p) * f(k)) > 0:
         return 'err', 'err'
 
     iter = 1
@@ -16,21 +16,27 @@ def bisekcja(f, p, k, cond, limiter):
             if abs(f(x)) < limiter:
                 return x, iter
 
-        if f(p) * f(x) > 0:
+        if f(x) * f(k) < 0:
             p = x
-        else:
+
+        elif f(x) * f(p) < 0:
             k = x
+
+        # if f(p) * f(x) > 0:
+        #     p = x
+        # else:
+        #     k = x
 
         iter += 1
 
 
 def sieczne(f, p, k, cond, limiter):
-    if (f(p) * f(k)) < 0:
+    if (f(p) * f(k)) > 0:
         return 'err', 'err'
 
     iter = 1
     while True:
-        x = p - (f(p) / (f(k) - f(p))) * (k - p)
+        x = p - (f(p) * (k - p)) / (f(k) - f(p))
         if cond == 1:
             if iter == limiter:
                 return x, iter
