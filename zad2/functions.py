44c7f16ce02059e67237e694fb36e8f6ffcaa7a5
236659 Mikołaj Stępniak <236659@edu.p.lodz.pl>
2022-04-05 15:59:25 +0200

    Already working Task 2

--- zad2/functions.py
+++ zad2/functions.py
@@ -2,31 +2,31 @@ import numpy as np
 from numpy.linalg import matrix_rank
 
 
-# def switch_rows(A, i, j):
-#     n = A.shape[0]
-#     E = np.eye(n)
-#     E[i, i] = 0
-#     E[j, j] = 0
-#     E[i, j] = 1
-#     E[j, i] = 1
-#     return E @ A
-#
-#
-# def scale_row(A, k, i):
-#     n = A.shape[0]
-#     E = np.eye(n)
-#     E[i, i] = k
-#     return E @ A
-#
-#
-# def add_row(A, k, i, j):
-#     n = A.shape[0]
-#     E = np.eye(n)
-#     if i == j:
-#         E[i, i] = k + 1
-#     else:
-#         E[i, j] = k
-#     return E @ A
+def switch_rows(A, i, j):
+    n = A.shape[0]
+    E = np.eye(n)
+    E[i, i] = 0
+    E[j, j] = 0
+    E[i, j] = 1
+    E[j, i] = 1
+    return E @ A
+
+
+def scale_row(A, k, i):
+    n = A.shape[0]
+    E = np.eye(n)
+    E[i, i] = k
+    return E @ A
+
+
+def add_row(A, k, i, j):
+    n = A.shape[0]
+    E = np.eye(n)
+    if i == j:
+        E[i, i] = k + 1
+    else:
+        E[i, j] = k
+    return E @ A
 
 
 def check(A, b):
@@ -43,17 +43,43 @@ def check(A, b):
             return "indeterminate"
 
 
-def gaussian(A, b):
-    n = len(b)  # number of rows
+def diagonally_dominant(A, b):
+    n = len(A)
 
-    for k in range(0, n - 1):  # rows
+    flag = True
+    for j in range(n):
+        max_, row = A[0][j], 0
+        for i in range(1, n):
+            if abs(A[i][j]) > abs(max_):
+                max_, row = A[i][j], i
+        A = switch_rows(A, j, row)
+        b = switch_rows(b, j, row)
+
+    for j in range(n):
+        max_ = A[j][j]
+        for i in range(j + 1, n):
+            if abs(max_) == abs(A[i][j]):
+                flag = False  # macierz nie jest przekatniowo dominujaca
+
+    return A, b, flag
+
+
+def gaussian(A, b):
+    n = len(A)
+    flag = True
+
+    A, b, flag = diagonally_dominant(A, b)
+
+    n = len(b)
+    for k in range(n - 1):  # rows
         for i in range(k + 1, n):  # columns
-            if A[i, k] != 0.0:
-                factor = A[i, k] / A[k, k]
-                A[i, k + 1:n] = A[i, k + 1:n] - np.multiply(factor, A[k, k + 1:n])
-                b[i] -= np.multiply(factor, b[k])
+
+            s = A[i, k] / A[k, k]
+            for j in range(k, n):
+                A[i, j] -= s * A[k, j]
+            b[i] -= s * b[k]
 
     for k in range(n - 1, -1, -1):  # rows
         b[k] = (b[k] - np.dot(A[k, k + 1:n], b[k + 1:n])) / A[k, k]
 
-    return b
+    return b, flag
--- zad2/main.py
+++ zad2/main.py
@@ -2,23 +2,37 @@
 
 from choice import file_choice
 from functions import gaussian, check
+import time
 
-A, b = file_choice()
-system_type = check(A, b)
 
-match system_type:
-    case "determinate":
-        print("Uklad rownan ma dokładnie jedno rozwiązanie:")
-        x = gaussian(A, b)
+if __name__ == '__main__':
+    A, b = file_choice()
+    system_type = check(A, b)
 
-        result = ""
-        for i in range(len(x)):
-            if i != 0:
-                result += ", "
-            result += f"x{i + 1} = {x[i]}"
+    match system_type:
+        case "determinate":
+            start = time.time_ns()
+            x, flag = gaussian(A, b)
+            total = (time.time_ns() - start) / 1000000
+            print(f"Czas pracy algorytmu eliminacji Gaussa: {total}ms\n")
 
-        print(result)
-    case "indeterminate":
-        print("Uklad rownan jest nieoznaczony")
-    case "contradictory":
-        print("Uklad rownan jest sprzeczny!")
+            if flag is True:
+                print(f"Macierz jest przekatniowo dominujaca!")
+            else:
+                print(f"Macierz nie jest przekatniowo dominujaca!")
+                print(f"(Uzyskane wyniki mogą byc bledne")
+
+            print("Uklad rownan ma dokładnie jedno rozwiązanie:")
+
+            result = ""
+            for i in range(len(x)):
+                if i != 0:
+                    result += ", "
+                result += f"x{i + 1} = {x[i]}"
+
+            print(result)
+
+        case "indeterminate":
+            print("Uklad rownan jest nieoznaczony")
+        case "contradictory":
+            print("Uklad rownan jest sprzeczny!")
