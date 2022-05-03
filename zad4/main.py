61f18c586b24a1ebda280b3782925eca8048e458
Mateusz <236630@edu.p.lodz.pl>
2022-05-03 14:29:22 +0200

    Sprawozdanie

--- zad4/main.py
+++ zad4/main.py
@@ -3,7 +3,8 @@
 # METODY NUMERYCZNE - ZAD 4
 
 from algorithms import Gauss, granica
-from choice import funkcjaa, dokladnosc
+from choice import funkcjaa, dokladnosc, metoda
+
 
 def main():
     print("Wybierz funkcje z ponizszych \n"
@@ -12,33 +13,32 @@ def main():
           "[3]. f(x) = x^2 + x + 1\n"
           "[4]. f(x) = sin(2x + 1) \n")
     fun = funkcjaa()
-    eps = dokladnosc()
-    # print("Wybierz metode \n"
-    #       "[1]. Newton-Cotes \n"
-    #       "[2]. Gauss-Czebyszew \n")
-    # met = metoda()
-    # if met == 1:
-    #     print("Wartosc dla Newtona-Cotesa: ")
-    #     print(granica(fun, eps))
-    # elif met == 2:
-    #     i = 2
-    #     while i != 6:
-    #         print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    #         print("Liczba wezlow: " + str(i))
-    #         print("Wartosc dla Gaussa-Czebyszewa: ")
-    #         print(Gauss(fun, i))
-    #         i += 1
-    # else:
-    #     print("Blad!")
-    # do sprawozdania by szybciej liczyc
-    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
-    i = 2
-    while i != 6:
+    print("Wybierz metode \n"
+          "[1]. Newton-Cotes \n"
+          "[2]. Gauss-Czebyszew \n")
+    met = metoda()
+    if met == 1:
+        eps = dokladnosc()
         print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-        print("Liczba wezlow: " + str(i))
-        print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
-        i += 1
+        print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
+    elif met == 2:
+        i = 2
+        while i != 6:
+            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+            print("Liczba wezlow: " + str(i))
+            print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
+            i += 1
+    else:
+        print("Blad!")
+    # # OBIE METODY OBLICZANE NA RAZ:
+    # print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    # print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
+    # i = 2
+    # while i != 6:
+    #     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    #     print("Liczba wezlow: " + str(i))
+    #     print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
+    #     i += 1
 
     return 0
 
