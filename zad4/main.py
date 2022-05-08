1c3553cc9a7e5ac202062209dca8acda95d422d8
Mateusz <236630@edu.p.lodz.pl>
2022-05-08 14:06:49 +0200

    poprawka w sprawku

--- zad4/main.py
+++ zad4/main.py
@@ -11,35 +11,34 @@ def main():
           "[1]. f(x) = sin(x) \n"
           "[2]. f(x) = 2x + 1 \n"
           "[3]. f(x) = x^2 + x + 1\n"
-          "[4]. f(x) = sin(2x + 1) \n")
-          # "[4]. f(x) = x^4 + 2x^3 - 2x^2 - x + 4 \n")
+          "[4]. f(x) = arcsin(x) \n")
     fun = funkcjaa()
     print("Wybierz metode \n"
           "[1]. Newton-Cotes \n"
           "[2]. Gauss-Czebyszew \n")
     met = metoda()
-    if met == 1:
-        eps = dokladnosc()
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-        print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
-    elif met == 2:
-        i = 2
-        while i != 6:
-            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-            print("Liczba wezlow: " + str(i))
-            print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
-            i += 1
-    else:
-        print("Blad!")
-    # # OBIE METODY OBLICZANE NA RAZ:
-    # print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    # print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
-    # i = 2
-    # while i != 6:
+    # if met == 1:
+    eps = dokladnosc()
     #     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    #     print("Liczba wezlow: " + str(i))
-    #     print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
-    #     i += 1
+    #     print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
+    # elif met == 2:
+    #     i = 2
+    #     while i != 6:
+    #         print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    #         print("Liczba wezlow: " + str(i))
+    #         print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
+    #         i += 1
+    # else:
+    #     print("Blad!")
+    # OBIE METODY OBLICZANE NA RAZ:
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    print("Wartosc dla Newtona-Cotesa: " + str(granica(fun, eps)))
+    i = 2
+    while i != 6:
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print("Liczba wezlow: " + str(i))
+        print("Wartosc dla Gaussa-Czebyszewa: " + str(Gauss(fun, i)))
+        i += 1
 
     return 0
 
